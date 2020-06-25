5e9e2d6f6058cf99940f1e473336c86989a38460
Iuliia <iuliiadmytryna@gmail.com>
2020-06-25 19:05:41 +0300

    components has been added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import PostStatusFilter from '../post-status-filter';
+import PostList from '../post-list';
 
 const App = () => {
-    return (<AppHeader/>)
+    return (
+        <div>
+            <AppHeader className = "app" />
+            <div className = "search-panel d-flex">
+                <SearchPanel/>
+                <PostStatusFilter/>
+            </div>
+            <PostList/>
+        </div>
+    )
 }
 
 export default App;
