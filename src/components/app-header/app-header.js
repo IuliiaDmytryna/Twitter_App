4a5a511d334905cc91f9d38ccca082e8f1744a1d
Iuliia <iuliiadmytryna@gmail.com>
2020-06-27 21:14:12 +0300

    styled-components css in js

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 import './app-header.css';
+
+const Header = styled.div`
+  align-items: flex-end;
+  justify-content: space-between;
+  display: flex;
+  h1 {
+    font-size: 26px;
+    color: ${props => props.colored ? 'red' : 'black'};
+    :hover{
+        color: green;
+    }
+  }
+  h2 {
+    font-size: 1.2rem;
+    color: grey;
+  }
+`
 
 const AppHeader = () => {
     return (
-        <div className = "app-header d-flex">
+        <Header colored>
             <h1>Iuliia Dmytryna</h1>
             <h2>5 записів, з них подобалося - 0</h2>
-        </div>
+        </Header>
     )
 }
 export default AppHeader;
