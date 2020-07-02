17ebe51b94508d14c506d6d080e0ea39898bdaf0
Iuliia <iuliiadmytryna@gmail.com>
2020-07-02 16:47:08 +0300

    continuation

--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+//import { Button } from 'reactstrap';
 import './post-status-filter.css';
 
 
 const PostStatusFilter = () => {
     return (
         <div className = "btn-group">
-            <Button outline color="info">info</Button>
+            {/* <Button outline color="info">info</Button>  з reactstrap*/}  
             <button type = "button" className = "btn btn-info">Все</button>
             <button type = "button" className = "btn btn-secondary">Сподобалося</button>
         </div>
