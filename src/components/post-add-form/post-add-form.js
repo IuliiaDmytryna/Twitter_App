3b2ceef2d0396d88402577f9e528542dd100131a
Iuliia <iuliiadmytryna@gmail.com>
2020-07-02 12:05:20 +0300

    creating posts has been implemented

--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './post-add-form.css';
 
-const PostAddForm = ({onAdd}) => {
+export default class PostAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        }
+        this.onValueChange = this.onValueChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+    onValueChange(e) {
+        this.setState({
+        text: e.target.value
+        })
+    }
+    onSubmit(e) {
+        e.preventDefault();              // не перезагружає сторінку після відправки форми
+        this.props.onAdd(this.state.text)
+        this.setState({                    //очищення імпута після введення поста
+            text: ''
+        })
+    }
+render(){
     return (
-        <div className = "bottom-panel d-flex">
+        <form className = "bottom-panel d-flex"
+        onSubmit = {this.onSubmit}
+        >
             <input
             type = "text"
             placeholder = "Відгук"
             className = "form-control new-post-lable"
+            onChange = {this.onValueChange}
+            value = {this.state.text}             //елемент стає контрольований (* до очищення імпута)
             />
             <button
             className = "btn btn-outline-secondary"
             type = "submit"
-            onClick = {() => onAdd("Hello!")}>
+            >
             Добавити
             </button>
-        </div>
+        </form>
     )
 }
-
-export default PostAddForm;
+}
